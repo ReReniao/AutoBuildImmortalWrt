a5f030b852d605bea651e33457f03396cfac0324
reniao <42791517+ReReniao@users.noreply.github.com>
2026-02-22 23:47:18 +0800

    Update build24.sh

--- mediatek-filogic/build24.sh
+++ mediatek-filogic/build24.sh
@@ -107,8 +107,12 @@ fi
 echo "$(date '+%Y-%m-%d %H:%M:%S') - Building image with the following packages:"
 echo "$PACKAGES"
 
-make image PROFILE=$PROFILE PACKAGES="$PACKAGES" FILES="/home/build/immortalwrt/files" ROOTFS_SIZE=${{ github.event.inputs.profile || '512' }}
-
+make image \
+  PROFILE="$PROFILE" \
+  PACKAGES="$PACKAGES" \
+  ROOTFS_SIZE="$ROOTFS_SIZE" \
+  FILES="/home/build/immortalwrt/files"
+  
 if [ $? -ne 0 ]; then
     echo "$(date '+%Y-%m-%d %H:%M:%S') - Error: Build failed!"
     exit 1
